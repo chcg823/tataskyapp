c9700b6b0c496fdd472da10d21edd9b8d53d08a9
Chittajallu Chinmai Ganesh <chinmai823@gmail.com>
2022-03-30 21:45:27 +0530

    Added reponse entity

--- src/main/java/com/cg/apps/tataskyapp/controller/AccountController.java
+++ src/main/java/com/cg/apps/tataskyapp/controller/AccountController.java
@@ -6,13 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.cg.apps.tataskyapp.entities.Account;
 import com.cg.apps.tataskyapp.service.AccountService;
@@ -33,34 +27,21 @@ public class AccountController {
 		return new ResponseEntity<String>(message, header, HttpStatus.OK);
 	}
 
-//	@PostMapping("/find-account")
-//	public ResponseEntity<String> findAccountById(@RequestBody Long accountId) {
-//		Account acc = accountService.findById(accountId);
-//		String message = acc.toString();
-//		HttpHeaders header = new HttpHeaders();
-//		header.add("desc", "find account by Id");
-//		return new ResponseEntity<String>(message, header, HttpStatus.OK);
-//	}
-
 	@GetMapping("/find-account/{accountId}")
-	public ResponseEntity<String> findAccountById(@PathVariable Long accountId) {
-		String message = new String();
+	public ResponseEntity<String> deleteAccountById(@PathVariable Long accountId) {
+		accountService.deleteByAccountId(accountId);
 		HttpHeaders header = new HttpHeaders();
-		header.add("desc", "find account by Id");
+		header.add("desc","delete account by id");
+		String message = "Account deleted";
 		return new ResponseEntity<String>(message, header, HttpStatus.OK);
 	}
 
-	@PostMapping("/delete-account")
-	public ResponseEntity<String> deleteAccountByAccountId(@RequestBody Long accountId) {
-		Optional<Account> accList = Optional.of(accountService.findByAccountId(accountId));
-		String message = new String();
-		if (accList.isEmpty())
-			message = "Account not found!";
-		else {
-			accountService.deleteByAccountId(accountId);
-			message = "Account deleted";
-		}
+	@PutMapping("/update")
+	public ResponseEntity<String> updateAccount(@RequestBody Account account) {
+		accountService.update(account);
 		HttpHeaders header = new HttpHeaders();
+		header.add("desc", "update account");
+		String message = "Account updated";
 		return new ResponseEntity<String>(message, header, HttpStatus.OK);
 	}
 
